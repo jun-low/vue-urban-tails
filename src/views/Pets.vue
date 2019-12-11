8e6834b9c9ce9afa1a2166fdea5c71ef7ee96627
jun-low <zhijun.low@gmail.com>
2019-12-11 12:13:21 +0800

    create dog components

--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container grid-list-md fluid>
     <v-layout wrap>
-      <v-flex xs12 sm4 md3>
+      <!-- <v-flex xs12 sm4 md3>
         <v-card color="grey lighten-2">
           <v-img src="https://goo.gl/6CQNDo" height="170px"> </v-img>
           <v-card-title>
@@ -10,14 +10,27 @@
             </div>
           </v-card-title>
         </v-card>
+      </v-flex> -->
+      <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
+        <app-dog :dog="pet"></app-dog>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import { Dogs } from "../data/dogs";
+import Dog from "../components/Dog.vue";
+
   export default {
-    
+    components: {
+      appDog: Dog
+    },
+    data () {
+      return{
+        dogs: Dogs
+      };
+    }
   }
 </script>
 
