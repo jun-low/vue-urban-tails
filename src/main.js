37c6255d127380223b9a522cef724b39d80a5307
jun-low <zhijun.low@gmail.com>
2019-12-11 10:34:54 +0800

    added vue router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from "./plugins/vuetify";
+import vueRouter from 'vue-router';
 
 new Vue({
   el: '#app',
   vuetify,
+  vueRouter,
   render: h => h(App)
 })
