c93893d91110d9a407a5d06e05c4272b48d1184e
jun-low <zhijun.low@gmail.com>
2019-12-13 12:27:30 +0800

    added vuex

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from "./plugins/vuetify";
-import vueRouter from 'vue-router';
 
 import Home from './views/Home';
 import Pets from './views/Pets'
 import VueRouter from 'vue-router';
 
-Vue.use(vueRouter);
+Vue.use(VueRouter);
 const routes = [
   {
     path: '/',
