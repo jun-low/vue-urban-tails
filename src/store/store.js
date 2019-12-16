a0aaaa3a05dc6324632f5ae7f3f09c5897c7fca9
jun-low <zhijun.low@gmail.com>
2019-12-16 15:50:22 +0800

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -25,7 +25,23 @@ export default new Vuex.Store({
   },
   mutations: {
     addToFavorites(state, payload) {
-      state.favorites.push(payload);
+      if (!state.favorites.includes(payload)) {
+          state.favorites.push(payload);
+          alert("Added to Favorite list!")
+        } else {
+          alert("This pet is already in our Favorite list!")
+        }
+    },
+    removeFromFavorites(state, payload) {
+      state.favorites.splice(state.favorites.indexOf(payload), 1);
+    }
+  },
+  actions: {
+    addToFavorites({ commit }, payload) {
+      commit("addToFavorites", payload);
+    },
+    removeFromFavorites({ commit }, payload) {
+      commit("removeFromFavorites", payload);
     }
   }
 });
